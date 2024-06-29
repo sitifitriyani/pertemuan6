1ef95d56fcec89a802c13349fa003d4f1b5a704b
fitri <sitifitriani8910@gmail.com>
2024-06-29 20:22:36 +0700

    second commit

--- src/components/ListProduct.jsx
+++ src/components/ListProduct.jsx
@@ -1,8 +1,48 @@
-export default function ListProduct({product}) {
-    return(
-        <div>
-            <h1>{product.name}</h1>
-            <p>{product.harga}</p>
-        </div>
-    )
+import { useState } from "react";
+
+export default function ListProduct({
+  product,
+  handleEditProduct,
+  handleDeleteProduct,
+}) {
+  const [edit, setEdit] = useState(false);
+  const [name, setName] = useState(product.name);
+  const [harga, setHarga] = useState(product.harga);
+
+  const handleSave = () => {
+    handleEditProduct(product.id, name, harga);
+    setEdit(false);
+  };
+  return (
+    <div className="border border-black p-3">
+      {!edit ? (
+        <>
+          <h1>{product.name}</h1>
+          <p>{product.harga}</p>
+          <div className=" flex justify-around gap-4">
+            <button onClick={() => setEdit(true)} className="bg-cyan-700 p-1 rounded-md">
+              edit
+            </button>
+            <button onClick={handleDeleteProduct} className="bg-cyan-700 p-1 rounded-md">
+              delete
+            </button>
+          </div>
+        </>
+      ) : (
+        <>
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <input
+            type="number"
+            value={harga}
+            onChange={(e) => setHarga(e.target.value)}
+          />
+          <button onClick={handleSave}>save</button>
+        </>
+      )}
+    </div>
+  );
 }
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,35 +1,75 @@
-import { useState } from "react"
-import ListProduct from "../components/ListProduct"
-
+import { useState } from "react";
+import ListProduct from "../components/ListProduct";
 
 export default function Products() {
-    const [products,setProducts] = useState ([
-        {
-            id : 1,
-            name : "sandal",
-            harga : 40000
-        },
-        {
-            id : 2,
-            name : "sepatu",
-            harga : 400000
-        },
-        ]
-    )
-    return(
-        <div>
+  const [products, setProducts] = useState([
+    {
+      id: 1,
+      name: "sandal",
+      harga: 40000,
+    },
+    {
+      id: 2,
+      name: "sepatu",
+      harga: 400000,
+    },
+  ]);
+  
+  const [productName, setProductName] = useState("");
+  const [productHarga, setProductHarga] = useState("");
+  const handleAddProduct = () => {
+    if (!productName || !productHarga) return;
+    const newProduct = {
+      id: products.length + 1,
+      name: productName,
+      harga: productHarga,
+    };
+    setProducts([...products, newProduct]);
+    productName("");
+    productHarga("");
+  };
 
-            <div>
-                {products.map((p)=>(
-                    <ListProduct key={p.id} 
-                    product={p}/>
-                ))}
-            </div>
+  const handleEditProduct = (id, newName, newHarga) => {
+    const updatedProducts = products.map((product) =>
+      product.id === id
+        ? { ...product, name: newName, harga: newHarga }
+        : product
+    );
+    setProducts(updatedProducts);
+  };
 
-            <input type="text" className=" border rounded-md"/>
-            <label htmlFor="text"></label>
-            <input type="number" />
-            <button>Tambah</button>
-        </div>
-        )
+  const handleDeleteProduct = (id) => {
+    const deleteProduct = products.filter((product) => product.id !== id);
+    setProducts(deleteProduct);
+  };
+  return (
+    <div>
+      <div className="flex gap-4 p-7 ">
+        {products.map((p) => (
+          <ListProduct
+            key={p.id}
+            product={p}
+            handleEditProduct={handleEditProduct}
+            handleDeleteProduct={() => handleDeleteProduct(p.id)}
+          />
+        ))}
+      </div>
+
+      <input
+        type="text"
+        placeholder="product name"
+        className=" border border-black rounded-md"
+        value={productName}
+        onChange={(e) => setProductName(e.target.value)}
+      />
+      <input
+        type="number"
+        placeholder="product harga"
+        className=" border border-black rounded-md"
+        value={productHarga}
+        onChange={(e) => setProductHarga(e.target.value)}
+      />
+      <button onClick={handleAddProduct}>Tambah</button>
+    </div>
+  );
 }
